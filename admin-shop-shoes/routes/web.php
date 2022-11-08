67a40b3c2f1a7582ae7c3f29a207deac3fab4c39
tuannm <tuannm@miichisoft.net>
2022-11-08 17:26:18 +0700

    admin laravel 9 and FE vuejs

---
+++ admin-shop-shoes/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['as'=>'admin', 'prefix'=>'admin'], function () {
+    Route::get('/', function (){
+        return view('dashboard');
+    })->name('.dashboard');
+    Route::get('/basic-table', function (){
+        return view('basic-table');
+    })->name('.basic-table');
+    Route::get('/map', function (){
+        return view('map');
+    })->name('.map');
+    Route::get('/profile', function (){
+        return view('profile');
+    })->name('.profile');
+});
+
